92ade31d64439303f31b47eb622d559851354e39
Andriy Chaika <andriy+en@chaika.in.ua>
2013-02-16 14:40:51 +0200

    Missing files fix

---
+++ NameCaseLib/Core/LastCache.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NameCaseLib.Core
+{
+    class LastCache
+    {
+        public void Push(String letters, int length, int stopAfter)
+        {
+
+        }
+
+        public String Get(int length, int stopAfter)
+        {
+            return "";
+        }
+    }
+}
